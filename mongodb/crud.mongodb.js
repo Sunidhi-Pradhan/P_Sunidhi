c0420adf8b7e764e21877a08998194b41c447a16
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-06-19 19:39:21 +0530

    insert operation

--- mongodb/crud.mongodb.js
+++ mongodb/crud.mongodb.js
@@ -1,3 +1,85 @@
 use("crudDB")
 // console.log(db)
 db.createCollection("courses")
+
+db.courses.insertOne({
+    name: "MongoDB Basics",
+    description: "Learn the basics of MongoDB",
+    price:0,
+    assignments:12,
+    projects:30
+});
+
+db.courses.insertMany([
+  {
+    "name": "MongoDB Basics",
+    "description": "Learn the basics of MongoDB",
+    "price": 0,
+    "assignments": 12,
+    "projects": 30
+  },
+  {
+    "name": "JavaScript Mastery",
+    "description": "Become proficient in JavaScript",
+    "price": 499,
+    "assignments": 20,
+    "projects": 15
+  },
+  {
+    "name": "React for Beginners",
+    "description": "Understand React.js fundamentals",
+    "price": 699,
+    "assignments": 10,
+    "projects": 5
+  },
+  {
+    "name": "Python Programming",
+    "description": "Complete Python programming course",
+    "price": 299,
+    "assignments": 25,
+    "projects": 10
+  },
+  {
+    "name": "Node.js Essentials",
+    "description": "Build backend with Node.js",
+    "price": 599,
+    "assignments": 15,
+    "projects": 12
+  },
+  {
+    "name": "Full Stack Web Dev",
+    "description": "Learn to build full-stack applications",
+    "price": 999,
+    "assignments": 30,
+    "projects": 20
+  },
+  {
+    "name": "Data Structures & Algorithms",
+    "description": "Master DSA for interviews",
+    "price": 399,
+    "assignments": 35,
+    "projects": 8
+  },
+  {
+    "name": "Machine Learning Intro",
+    "description": "Get started with ML concepts",
+    "price": 799,
+    "assignments": 18,
+    "projects": 7
+  },
+  {
+    "name": "Database Design",
+    "description": "Learn to design efficient databases",
+    "price": 249,
+    "assignments": 14,
+    "projects": 10
+  },
+  {
+    "name": "Git & GitHub Crash Course",
+    "description": "Version control with Git and GitHub",
+    "price": 0,
+    "assignments": 6,
+    "projects": 3
+  }
+]
+);
