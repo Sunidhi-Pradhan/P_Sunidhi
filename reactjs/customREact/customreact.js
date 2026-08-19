8b144dab33add6ac97aa89d4b7727707d1bbf0d7
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2026-08-19 12:22:22 +0530

    refactor: streamline customRender function for improved attribute handling

--- reactjs/customREact/customreact.js
+++ reactjs/customREact/customreact.js
@@ -1,9 +1,18 @@
 function customRender(reactElement, container){
-    const DomElement = document.createElement(reactElement.type)
-    DomElement.innerHTML = reactElement.children
-    DomElement.setAttribute('href', reactElement.props.href)
-    DomElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(DomElement)
+    // const DomElement = document.createElement(reactElement.type)
+    // DomElement.innerHTML = reactElement.children
+    // DomElement.setAttribute('href', reactElement.props.href)
+    // DomElement.setAttribute('target', reactElement.props.target)
+    // container.appendChild(DomElement)
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props){
+        if(prop == 'children') continue
+        domElement.setAttribute(prop, reactElement.props[prop])
+        
+    }
+    container.appendChild(domElement)
 }
 
 const reactElement = {
